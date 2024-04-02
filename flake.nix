5d6f6d3a8521851fecf53fb385be5f9a3802d8b0
jokatzke <jokatzke@fastmail.com>
2024-04-02 11:25:53 +0200

    refactor(nix): optionally provide python library through overlay

--- flake.nix
+++ flake.nix
@@ -15,99 +15,111 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    {
-      # define an overlay to add text-extraction to nixpkgs
-      overlays.default = (final: prev: {
-        inherit (self.packages.${final.system}) text-extraction;
-      });
-    } //
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        nix-filter = self.inputs.nix-filter.lib;
-        openapi-checks = self.inputs.openapi-checks.lib.${system};
-        python = pkgs.python3;
+    let
+      nix-filter = self.inputs.nix-filter.lib;
 
-        ### list of python packages required to build / run the application
-        python-packages-build = py-pkgs:
-          with py-pkgs; [
-            setuptools
-            pandas
-            numpy
-            fastapi
-            uvicorn
-            pydantic
-            trafilatura
-            py3langid
-            playwright
-            # the version of pyrate-limiter in nixpkgs is old, so override it
-            (pyrate-limiter.overrideAttrs (oldAttrs: rec {
-              version = "3.6.0";
-              # the name does not properly update by just changing the version
-              name = "pyrate-limiter-${version}";
-              src = oldAttrs.src.override {
-                rev = "refs/tags/v${version}";
-                hash = "sha256-I/wgHVm3QMgt5KEEJnjMj0eH7LTIlNxifKnHqfH4VzA=";
-              };
-            }))
-          ];
+      ### list of python packages required to build / run the application
+      python-packages-build = py-pkgs:
+        with py-pkgs; [
+          setuptools
+          pandas
+          numpy
+          fastapi
+          uvicorn
+          pydantic
+          trafilatura
+          py3langid
+          playwright
+          # the version of pyrate-limiter in nixpkgs is old, so override it
+          (pyrate-limiter.overrideAttrs (oldAttrs: rec {
+            version = "3.6.0";
+            # the name does not properly update by just changing the version
+            name = "pyrate-limiter-${version}";
+            src = oldAttrs.src.override {
+              rev = "refs/tags/v${version}";
+              hash = "sha256-I/wgHVm3QMgt5KEEJnjMj0eH7LTIlNxifKnHqfH4VzA=";
+            };
+          }))
+        ];
 
-        ### list of python packages to include in the development environment
-        # the development installation contains all build packages,
-        # plus some additional ones we do not need to include in production.
-        python-packages-devel = py-pkgs:
-          with py-pkgs; [
-            ipython
-            black
-            pyflakes
-            isort
-          ]
-          ++ (python-packages-build py-pkgs);
+      ### list of python packages to include in the development environment
+      # the development installation contains all build packages,
+      # plus some additional ones we do not need to include in production.
+      python-packages-devel = py-pkgs:
+        with py-pkgs; [
+          ipython
+          black
+          pyflakes
+          isort
+        ]
+        ++ (python-packages-build py-pkgs);
 
-        ### create the python package
-        # the library, parameterized by the python version to use
-        text-extraction-library = py-pkgs: py-pkgs.buildPythonPackage {
-          pname = "text-extraction";
-          version = "0.2.0";
-          /*
+      ### create the python package
+      # the library, parameterized by the python version to use
+      get-text-extraction-lib = py-pkgs: py-pkgs.buildPythonPackage {
+        pname = "text-extraction";
+        version = "0.2.0";
+        /*
           only include files that are related to the application
           this will prevent unnecessary rebuilds
           */
-          src = nix-filter {
-            root = self;
-            include = [
-              # folders
-              "text_extraction"
-              "test"
-              # files
-              ./setup.py
-              ./requirements.txt
-            ];
-            exclude = [ (nix-filter.matchExt "pyc") ];
-          };
-          propagatedBuildInputs = (python-packages-build py-pkgs);
-          # this package has no tests
-          doCheck = false;
+        src = nix-filter {
+          root = self;
+          include = [
+            # folders
+            "text_extraction"
+            "test"
+            # files
+            ./setup.py
+            ./requirements.txt
+          ];
+          exclude = [ (nix-filter.matchExt "pyc") ];
         };
+        propagatedBuildInputs = (python-packages-build py-pkgs);
+        # this package has no tests
+        doCheck = false;
+      };
 
-        # convert the package built above to an application
-        # a python application is essentially identical to a python package,
-        # but without the importable modules. as a result, it is smaller.
-        text-extraction-service = python.pkgs.toPythonApplication (
-          (text-extraction-library python.pkgs).overrideAttrs
+      # convert the package built above to an application
+      # a python application is essentially identical to a python package,
+      # but without the importable modules. as a result, it is smaller.
+      get-text-extraction-app = pkgs: py-pkgs:
+        py-pkgs.toPythonApplication (
+          (get-text-extraction-lib py-pkgs).overrideAttrs
             (finalAttrs: prevAttrs: {
               # provide the browsers for playwright directly through nix
               makeWrapperArgs = [
                 "--set PLAYWRIGHT_BROWSERS_PATH ${pkgs.playwright-driver.browsers}"
               ];
             }));
-
-      in
+    in
+    {
+      overlays = {
+        # add the text-extraction library to nixpkgs
+        python-lib = (final: prev: {
+          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+            (python-final: python-prev: {
+              text-extraction = get-text-extraction-lib python-final;
+            })
+          ];
+        });
+        # add the webservice to nixpkgs
+        default = (final: prev: {
+          text-extraction = self.outputs.packages.${final.system}.default;
+        });
+      };
+    } //
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+        openapi-checks = self.inputs.openapi-checks.lib.${system};
+     in
       {
         # the packages that we can build
-        packages = {
-          text-extraction = text-extraction-service;
-          default = text-extraction-service;
+        packages = rec {
+          text-extraction = get-text-extraction-app pkgs python.pkgs;
+          default = text-extraction;
         };
         # the development environment
         devShells.default = pkgs.mkShell {
