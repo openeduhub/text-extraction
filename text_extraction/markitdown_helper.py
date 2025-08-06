0e927f352de69a9403ce0d54e6efb82d92e9773e
criamos <981166+Criamos@users.noreply.github.com>
2025-08-06 12:37:04 +0200

    refactor: make helper function easier to re-use and improve performance

--- text_extraction/markitdown_helper.py
+++ text_extraction/markitdown_helper.py
@@ -1,9 +1,12 @@
+from collections import namedtuple
 from os.path import splitext
 from urllib.parse import urlparse
 
 import requests
 from loguru import logger
 from markitdown import MarkItDown
+
+from text_extraction.grab_content import GrabbedContent, FailedContent
 
 # ToDo: currently unsupported document formats:
 #  - .odt (Text)
@@ -72,7 +75,7 @@ MARKITDOWN_SUPPORTED_FORMATS = {
 #       "application/vnd.oasis.opendocument.presentation",  # .odp
 #       "application/vnd.oasis.opendocument.spreadsheet",  # .ods
 # for reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types/Common_types
-MARKITDOWN_SUPPORTED_CONTENT_MIME_TYPES = [
+MARKITDOWN_SUPPORTED_CONTENT_MIME_TYPES: set[str] = {
     # PDF formats
     "application/pdf",
     # Word formats
@@ -100,7 +103,7 @@ MARKITDOWN_SUPPORTED_CONTENT_MIME_TYPES = [
     "xls",
     "ppt",
     "csv",
-]
+}
 
 
 def _detect_file_extension_in_url_path(url: str) -> str | None:
@@ -120,59 +123,72 @@ def _detect_file_extension_in_url_path(url: str) -> str | None:
         return None
 
 
-def file_extension_is_supported_by_markitdown(url: str) -> bool:
-    # 1) check if the URL path indicates a file extension
-    # 2) if not: check the content-type header
-    # ToDo (inaccurate/risky): 3) guess the file extension by using the response content (e.g. via magic bytes)
-    _file_extension_in_url_path = _detect_file_extension_in_url_path(url)
-    if _file_extension_in_url_path:
-        return True
-    else:
-        # fire a HEAD request (to safe traffic) and check the content-type header
-        # ToDo: some websites might not return a valid Response via HEAD requests -> fallback to GET request?
-        # ToDo: rate-limiting might be necessary here!
-        _request_head = requests.head(url)
+MarkItDownCompatible = namedtuple(
+    "MarkItDownCompatible", ["supported_by_markitdown", "http_status_code"]
+)
+
+
+def _content_type_is_supported_by_markitdown(url: str) -> MarkItDownCompatible:
+    # check the content-type header (as it's the most reliable way to detect the file extension)
+    # ToDo (inaccurate/risky): guess the file extension by using the response content (e.g. via magic bytes)?
+    _is_compatible: bool = False
+
+    # fire a HEAD request (to safe traffic) and check the content-type header
+    # ToDo: some websites might not return a valid Response via HEAD requests -> fallback to GET request?
+    # ToDo: rate-limiting might be necessary here!
+    _request_head = requests.head(url)
+    _status_code = _request_head.status_code
+    if _request_head.ok:
+        # checking the Content-Type header only makes sense if the HTTP status code is in the >= 200 and < 400 range
         if "Content-Type" in _request_head.headers:
             _content_type = _request_head.headers["Content-Type"]
-            for _supported_content_mime_type in MARKITDOWN_SUPPORTED_CONTENT_MIME_TYPES:
+            if _content_type in MARKITDOWN_SUPPORTED_CONTENT_MIME_TYPES:
                 # this detection will not work for websites that return a generic
                 # "application/octet-stream" content-type while embedding/rendering the binary file in the DOM
-                if _supported_content_mime_type in _content_type:
-                    logger.debug(
-                        f"Supported content-type in URL {url} detected: {_content_type}"
-                    )
-                    return True
-    return False
+                logger.debug(
+                    f"Supported content-type in URL {url} detected: {_content_type}"
+                )
+                _is_compatible = True
+    _result = MarkItDownCompatible(
+        supported_by_markitdown=_is_compatible,
+        http_status_code=_status_code,
+    )
+    return _result
 
 
-def convert_to_markdown(url: str) -> str | None:
+def _fetch_markdown_from_url(
+    url: str, mark_it_down_compatible=MarkItDownCompatible
+) -> GrabbedContent | FailedContent:
     _md_converter = MarkItDown()
     try:
         _md_result = _md_converter.convert(url)
-        return _md_result.markdown
+        grabbed_content: GrabbedContent = GrabbedContent(
+            fulltext=_md_result.markdown,
+            status=mark_it_down_compatible.http_status_code,
+        )
+        return grabbed_content
     except Exception as e:
         logger.error(f"Markdown conversion failed for URL {url}. \nReason: {e}")
-        return None
+        _failed_content: FailedContent = FailedContent(
+            content=None,
+            reason=f"Markdown conversion failed for URL {url}. \n Website target provided the following reason: {e}",
+            status=mark_it_down_compatible.http_status_code,
+        )
+        return _failed_content
+
+
+def fetch_markdown_from_url(url: str) -> GrabbedContent | FailedContent:
+    _is_file_extension_supported = _content_type_is_supported_by_markitdown(url)
+    _result: GrabbedContent | FailedContent = _fetch_markdown_from_url(
+        url=url,
+        mark_it_down_compatible=_is_file_extension_supported,
+    )
+    return _result
 
 
 # ToDo: implement functions for
-#  - retrieve binary files directly with markitdown
 #  - implement markitdown function in webservice.py
 #   - rate-limit markitdown so it doesn't hammer websites with too many requests at the same time
 
 if __name__ == "__main__":
-    url_testcases = [
-        "https://www.example.com/example.pdf",
-        "https://www.example.com/example.docx",
-        "https://www.example.com/example.docx?download=full",
-        # "https://ocw.mit.edu/courses/esd-290-special-topics-in-supply-chain-management-spring-2005/060a50539c55fd852582f0ac63844a3e_berniehogan.pdf",
-        # "https://www.zoerr.de/edu-sharing/eduservlet/download?nodeId=2c9d9f08-3caa-4bd6-bf4b-0c9b5fb0aacc",
-    ]
-    for url_testcase in url_testcases:
-        file_extension_is_supported = file_extension_is_supported_by_markitdown(
-            url_testcase
-        )
-    convert_to_markdown(
-        url="https://www.umwelt-im-unterricht.de/hintergrund/umweltpolitik-in-der-ddr-und-die-entwicklung-seit-der-friedlichen-revolution-1989-1990"
-    )
     pass
